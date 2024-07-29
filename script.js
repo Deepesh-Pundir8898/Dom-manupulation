32af402608c0385f64b0fe091655444cb04f30fa
Deepesh-Pundir <deepeshpundir@gmail.com>
2024-07-29 10:12:32 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const myDiv =document.getElementById("my-div");
+
+
+myDiv.style.color="blue";
+myDiv.style.backgroundColor="yellow";
+myDiv.style.margin="20px";
+myDiv.style.padding="10px";
+myDiv.style.fontSize="18px";
+myDiv.style.fontWeight="bold";
+myDiv.style.height="200px";
+myDiv.style.width="300px";
